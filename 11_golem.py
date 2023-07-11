124cefd393f001f590c62b9ec615f3a180608ffa
Sunnie <51373309+sunnie39@users.noreply.github.com>
2023-07-11 10:31:34 +0900

    Update 11_golem.py

--- 11_golem.py
+++ 11_golem.py
@@ -6,7 +6,7 @@
 import requests
 
 url ="https://los.rubiya.kr/chall/golem_4b5202cfedd8160e73124b5234235ef5.php?" #공격URL
-cookies ={"PHPSESSID": "5mc7krftsj3sgtp7h7ofqnnr0a"} #쿠키값
+cookies ={"PHPSESSID": ""} #쿠키값
 
 #패스워드 길이 찾기
 def pw_len():
