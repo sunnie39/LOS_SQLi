d1fde57e68ae7f3b6de21da62bedcf6a28c3aad3
Sunnie <51373309+sunnie39@users.noreply.github.com>
2023-07-11 10:30:47 +0900

    Update 23_hell_fire.py

--- 23_hell_fire.py
+++ 23_hell_fire.py
@@ -5,7 +5,7 @@ import time
 import requests
 
 url ="https://los.rubiya.kr/chall/hell_fire_309d5f471fbdd4722d221835380bb805.php?" #공격URL
-cookies ={"PHPSESSID": "l18liqrmt5j8f7b5386fj62t8t"} #쿠키값
+cookies ={"PHPSESSID": ""} #쿠키값
 
 #패스워드 길이 찾기
 def pw_len():
